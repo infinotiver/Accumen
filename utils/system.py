e322e4e14ff64edcb0c5fd365f0ae1a15d13427e
prakarsh17 <prakarsh.3482@gmail.com>
2023-12-31 17:10:04 +0000

    [RELEASE] Updated gitigore and removed useless files

--- utils/system.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-
-def restart_bot(mode):
-    if mode == 1:
-        os.execv(sys.executable, ["python"] + sys.argv)
-    if mode == 2:
-        os.system("busybox reboot")
--- venv/share/doc/networkx-3.1/examples/drawing/README.txt
+++
@@ -1,1 +0,0 @@
-/home/runner/.cache/pip/pool/0e/85/8c/703082fd34e3444a81a9cfd831ab484cfd7fda51e9ed1bf9231accd9b0
