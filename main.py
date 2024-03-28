e7ad693fd02fbbb07d7089bee3d081d2f95bf65c
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-03-28 11:00:59 +0530

    Reformat main.py

--- main.py
+++ main.py
@@ -25,14 +25,15 @@ load_dotenv()
 
 # [Setup] Discord Bot Client Setup
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="a!", intents=intents,help_command=PrettyHelp())
+client = commands.Bot(command_prefix="a!", intents=intents, help_command=PrettyHelp())
 
 # [Setup] MongoDB
 mongo_url = os.environ.get("mongodb")
 cluster = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
 incoming = cluster["accumen"]["incoming"]
 
-#[Main]
+# [Main]
+
 
 @client.event
 async def on_ready():
@@ -48,12 +49,12 @@ async def on_ready():
             "light_blue",
         )
     )
-        # Define the base directory where your project resides
+    # Define the base directory where your project resides
     base_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the path to the "cogs" directory relative to the base directory
-    cogs_dir = os.path.join(base_dir,  "cogs")  
-    
+    cogs_dir = os.path.join(base_dir, "cogs")
+
     print(cogs_dir)
     for filename in os.listdir(cogs_dir):
         if filename.endswith(".py"):
@@ -64,29 +65,29 @@ async def on_ready():
                 print(colored(f"[-] Not Loaded {filename}\n {e}", "red"))
 
     system_latency = round(client.latency * 1000)
-    ctime=datetime.datetime.now()
+    ctime = datetime.datetime.now()
     unixtime = time.mktime(ctime.timetuple())
-    view=ubuttons.developer_controls()
+    view = ubuttons.developer_controls()
     em = dembed(title=f"{client.user.name} is online !")
     em.set_thumbnail(url=client.user.avatar.url)
     em.add_field(name="Ping", value=f"{system_latency} ms", inline=False)
     em.add_field(name="Servers", value=f"{len(client.guilds)}", inline=True)
-    last_server_count=len(client.guilds)
+    last_server_count = len(client.guilds)
     em.add_field(name="Users", value=f"{len(client.users)}", inline=True)
-    em.add_field(name="Started",value=f"<t:{int(unixtime)}:R>",inline=False)
-    channel=client.get_channel(1197514010388611102)
-    await channel.purge(limit=100) 
-    msg=await channel.send(embed=em,view=view)
-    #client.add_dynamic_items(ubuttons.dynamic_add_answer)
-    #client.add_dynamic_items(ubuttons.dynamic_upvote)
-    #client.add_dynamic_items(ubuttons.dynamic_report)
+    em.add_field(name="Started", value=f"<t:{int(unixtime)}:R>", inline=False)
+    channel = client.get_channel(1197514010388611102)
+    await channel.purge(limit=100)
+    msg = await channel.send(embed=em, view=view)
+    # client.add_dynamic_items(ubuttons.dynamic_add_answer)
+    # client.add_dynamic_items(ubuttons.dynamic_upvote)
+    # client.add_dynamic_items(ubuttons.dynamic_report)
     client.add_view(ubuttons.answercontrol())
     client.add_view(ubuttons.Qrscontrol())
     print(colored("[+] Persistent views and Dynamic items ", "light_blue"))
-    dev_guild =976878887004962917 
+    dev_guild = 976878887004962917
     await client.tree.sync(guild=discord.Object(dev_guild))
     await client.tree.sync()
- 
+
 
 # [Setup] Running discord bot along with flask app
 keep_alive()
