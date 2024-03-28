72a51a745bf2e54e6366392e516545b5b6c0bc06
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-03-28 10:49:42 +0530

    Add indecent threshold

--- utils/automod.py
+++ utils/automod.py
@@ -22,6 +22,7 @@ def text_moderation(text):
           "threat": 0.8,
           "offensive": 0.95,
           "erotic": 0.7,
+          "indecent":1, # Practically impossible
           "spam": 0.95  # Higher threshold for spam to avoid false positives
       }
 
