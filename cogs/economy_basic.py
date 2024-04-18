7eabb9a1f9972a624466821bf146c1a41fe39d2f
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-04-18 16:39:47 +0530

    Update coin and gem emojis to use raw string; update balance command method, format file

--- cogs/economy_basic.py
+++ cogs/economy_basic.py
@@ -24,29 +24,31 @@ mongo_url = os.environ["mongodb"]
 cluster = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
 economy_collection = cluster["accumen"]["economy"]
 
-coin_emoji = '<:c_:1090623584432570440>'
-gem_emoji = '<:d_:1090623594893160458>'
+coin_emoji = "<:c_:1090623584432570440>"
+gem_emoji = "<:d_:1090623594893160458>"
+
+
 class Economy_Basic(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
 
     @app_commands.command(name="balance", description="Shows your balance")
     async def balance(self, ctx, user: discord.Member = None):
         user = user or ctx.user
         try:
             bal = await economy_functions.get_user_data(user.id)
-            if bal is None:
-                await economy_functions.open_account(user.id)
-                bal = await economy_functions.get_user_data(user.id)
-            embed = (
-                dembed(title=f"{user.display_name}'s Balance", thumbnail=user.avatar.url)
+            embed = dembed(
+                title=f"{user.display_name}'s Balance", thumbnail=user.avatar.url
             )
-            embed.add_field(name="Coins", value=f"{coin_emoji} {bal['coins']}", inline=True)
-            embed.add_field(name="Gems", value=f"{gem_emoji} {bal['gems']}", inline=True)
+            embed.add_field(
+                name="Coins", value=f"{coin_emoji} {bal['coins']}", inline=True
+            )
+            embed.add_field(
+                name="Gems", value=f"{gem_emoji} {bal['gems']}", inline=True
+            )
             embed.set_footer(icon_url=ctx.user.avatar)
-            
+
             await ctx.response.send_message(embed=embed, ephemeral=True)
         except Exception as e:
             await ctx.response.send_message(f"An error occurred: {e}", ephemeral=True)
